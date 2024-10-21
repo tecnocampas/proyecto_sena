0dbe75de013c0cc5cd3851375842e9fad2923b2c
tecnocampas <jacaes@misena.edu.co>
2024-10-21 16:47:54 -0500

    Esta es la actualizacion del proyecto

--- proyrcto1/src/App.js
+++ proyrcto1/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="contenedor">
+     <button>
+      Soy un botón
+    </button>
+    
+    <h1>Hola Mundo De La Programacion</h1>
+    <p>En esta app estamos aprendiendo a desarrollar</p>
+
+    
+    
+</div>
+
+    
   );
 }
+
+
 
 export default App;
