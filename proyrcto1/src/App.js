6bb2d54db6c16d821dadcd0b7b74cb6b677e9196
tecnocampas <jacaes@misena.edu.co>
2024-10-21 17:28:17 -0500

    Este es el cuarto commit para la evidencia

--- proyrcto1/src/App.js
+++ proyrcto1/src/App.js
@@ -9,8 +9,13 @@ function App() {
       Soy un botón
     </button>
     
-    <h1>Hola Mundo De La Programacion</h1>
-    <p>En esta app estamos aprendiendo a desarrollar</p>
+    <h1>Hola Mundo De La Programacion</h1> 
+
+    <p>En esta app estamos aprendiendo a desarrollar
+       lo mas importante  de aprender, es tener las ganas  y la dedicacion asi
+       como la fe en que si vamos a aprender  y algo muy util es la practica
+    </p>
+    
 
     
     
@@ -19,6 +24,7 @@ function App() {
     
   );
 }
+
 
 
 
